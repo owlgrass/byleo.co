f27dff8c4d9aab60bdfcf32b20855f85d9f0165c
owlgrass <32409699+owlgrass@users.noreply.github.com>
2020-05-09 11:34:19 +0800

    Removed color background

--- js/main.js
+++ js/main.js
@@ -12,8 +12,8 @@ function getColors(n) {
 
 	// For the given lightness L, the highest chroma is C, in order for a complete Hue circle
 
-	// let L = 75
-	// let C = 59
+	let L = 75
+	let C = 59
 
 	// let L = 85
 	// let C = 34
@@ -26,12 +26,13 @@ function getColors(n) {
 
 	// let L = 97
 	// let C = 6
-	let L = 99
-	let C = 2
+
+	// let L = 99
+	// let C = 2
 
 	let colors = new Array(n).fill().map((e, i) => {
 
-		let H = i * 360 / 8
+		let H = i * 360 / n
 
 		let color = colorspaces.make_color('CIELCHuv', [L, C, H])
 		let [r, g, b] = color.as('sRGB').map(c => parseInt(c * 255) )
@@ -47,20 +48,35 @@ function getColors(n) {
 // }
 
 
-// Cycle background color randomly
-let $bg = document.querySelector('.main-background')
-let shuffling = false
-let colors = getColors(8)
-let colorIndex = 0
 
-document.addEventListener('click', e => {
-	if (shuffling) 
-		return
-	else
-		shuffling = true
 
-	window.setInterval(() => {
+// Create color table
+const interval = 100	// in ms
+const nColors = 12
+const $nav = document.querySelector('nav')
 
-		$bg.style.background = colors[colorIndex++ % 8]
-	}, 200)
+let colors = getColors(nColors)
+
+// Create table elements
+let rowLength = 6
+let $table = document.createElement('table')
+for (let tr = 0; tr < Math.ceil(nColors/rowLength); tr++) {
+	let $tr = document.createElement('tr')
+	for (let td = 0; td < rowLength; td++) {
+		let i = tr * rowLength + td
+		if (i >= nColors) break
+		let $td = document.createElement('td')
+		$tr.append($td)
+	}
+	$table.append($tr)
+}
+$nav.prepend($table)
+
+// Set table cells background
+let $cells = $table.querySelectorAll('td')
+$cells.forEach(($cell, i) => {
+	$cell.style.background = colors[i]
+	$cell.style.width = '6px'
+	$cell.style.height = '6px'
 })
+
